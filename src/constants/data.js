3ca020666b1a9b95ac4ec0e44db0d4751023dd59
abdulaziz Alhariri <abd828981@gmail.com>
2023-01-31 20:00:43 +0300

    resturant-files

---
+++ src/constants/data.js
@@ -0,0 +1,82 @@
+import images from './images';
+
+const Juice = [
+  {
+    title: 'Juice  Type  Here',
+    price: '$56',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Juice  Type  Here',
+    price: '$59',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Juice  Type  Here',
+    price: '$44',
+    tags: 'FR | 750 ml',
+  },
+  {
+    title: 'Juice  Type  Here',
+    price: '$31',
+    tags: 'CA | 750 ml',
+  },
+  {
+    title: 'Juice  Type  Here',
+    price: '$26',
+    tags: 'IE | 750 ml',
+  },
+];
+
+const cocktails = [
+  {
+    title: 'Juice  Type  Here',
+    price: '$20',
+    tags: 'AU | Bottle'
+  },
+  {
+    title: "Juice  Type  Here",
+    price: '$16',
+    tags: 'AU | Bottle'
+  },
+  {
+    title: 'Juice  Type  Here',
+    price: '$10',
+    tags: 'AU | Bottle'
+  },
+  {
+    title: 'Apple Juice',
+    price: '$31',
+    tags: 'AU | Bottle'
+  },
+  {
+    title: 'Orange Juice',
+    price: '$26',
+    tags: 'AU | Bottle'
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.award02,
+    title: 'Bib Gourmond',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award01,
+    title: 'Rising Star',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award05,
+    title: 'AA Hospitality',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award03,
+    title: 'Outstanding Chef',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+];
+
+export default { Juice, cocktails, awards };
